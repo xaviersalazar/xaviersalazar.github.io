870ccd546d589003c8fe142430ec6ea649e30921
Xavier Salazar <xavierssalazar92@gmail.com>
2022-08-24 12:08:58 -0500

    adding mobile-friendlyness

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => (
   <div className="w-full mx-auto my-0 text-center relative top-[40%] translate-y-[-50%]">
-    <h1 className="text-7xl font-black">I am</h1>
-    <h1 className="text-9xl font-black bg-clip-text text-fill-transparent bg-gradient-to-r from-[#ee0979] to-[#ff6a00]">
+    <h1 className="text-5xl md:text-7xl font-black">I am</h1>
+    <h1 className="text-8xl md:text-9xl font-black bg-clip-text text-fill-transparent bg-gradient-to-r from-[#ee0979] to-[#ff6a00]">
       Xavier Salazar.
     </h1>
   </div>
