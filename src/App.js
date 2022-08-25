dcb03dc32387991be0bb6a9424df292b8c77c044
Xavier Salazar <xavierssalazar92@gmail.com>
2022-08-25 11:05:56 -0500

    adding social links

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import {
 import Skills from "./Skills";
 
 const App = () => (
-  <div className="w-full mx-auto mt-8 mb-0 text-center">
+  <div className="w-full mx-auto mt-8 md:mt-10 xl:mt-20 mb-0 text-center">
     <h1 className="text-4xl md:text-7xl font-black">I am</h1>
     <h1 className="text-7xl md:text-9xl font-black bg-clip-text text-fill-transparent bg-gradient-to-r from-[#ee0979] to-[#ff6a00]">
       Xavier Salazar
     </h1>
-    <div className="p-8" id="dev-card-container">
+    <div className="p-8 mt-2" id="dev-card-container">
       <div
         className="flex flex-col h-[540px] max-w-[508px] shadow-3xl rounded-2xl mx-auto my-0 px-6 py-4 text-left relative overflow-hidden"
         id="dev-card"
@@ -29,9 +29,23 @@ const App = () => (
         </div>
         <Skills />
         <div className="flex gap-2 mt-auto z-50">
-          <IoLogoGithub className="text-2xl" />
-          <IoLogoLinkedin className="text-2xl" />
-          <IoMailOpenOutline className="text-2xl" />
+          <a
+            href="https://github.com/xaviersalazar"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IoLogoGithub className="text-2xl" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/xavier-salazar-488316227"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IoLogoLinkedin className="text-2xl" />
+          </a>
+          <a href="mailto:xavierssalazar92@gmail.com">
+            <IoMailOpenOutline className="text-2xl" />
+          </a>
         </div>
       </div>
     </div>
